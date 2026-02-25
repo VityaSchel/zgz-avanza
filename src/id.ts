d256303acb3c8646092b3a17c2e5a4750bb6f6a0
Viktor Shchelochkov <hi@hloth.dev>
2026-02-25 15:16:00 +0100

    Last byte of block 10 is XOR checksum

--- src/id.ts
+++ src/id.ts
@@ -7,8 +7,8 @@ export function decodeId(block: Uint8Array): string {
 	if (block.length !== 16) {
 		throw new Error(`Invalid block length: expected 16 bytes`);
 	}
-	const prefix = String.fromCharCode(...block.slice(0, 2));
-	let id = block.slice(2, 15);
+	const idPrefix = String.fromCharCode(...block.slice(0, 2));
+	let idNumber = block.slice(2, 15);
 	const aXor = block.slice(15, 16)[0];
 	const bXor = block.slice(0, 15).reduce((acc, byte) => acc ^ byte, 0);
 	if (aXor === undefined) {
@@ -17,10 +17,10 @@ export function decodeId(block: Uint8Array): string {
 	if (aXor !== bXor) {
 		throw new Error(`Invalid checksum byte: ${aXor}, expected ${bXor}`);
 	}
-	while (id[id.length - 1] === 0 && id.length > 3) {
-		id = id.slice(0, -1);
+	while (idNumber[idNumber.length - 1] === 0 && idNumber.length > 3) {
+		idNumber = idNumber.slice(0, -1);
 	}
-	return prefix + id.toHex();
+	return idPrefix + idNumber.toHex();
 }
 
 /**
@@ -28,17 +28,17 @@ export function decodeId(block: Uint8Array): string {
  * @param cardId The ID to encode
  * @return The encoded ID as a 16-byte block for block 2
  */
-export function encodeId(cardId: string): Uint8Array {
-	if (!/^[A-Z]{2}[0-9]{6,26}$/.test(cardId)) {
+export function encodeId(id: string): Uint8Array {
+	if (!/^[A-Z]{2}[0-9]{6,26}$/.test(id)) {
 		throw new Error(
-			`Invalid ID format: ${cardId}. Expected format is 2 uppercase letters followed by 6 to 26 digits.`,
+			`Invalid ID format: ${id}. Expected format is 2 uppercase letters followed by 6 to 26 digits.`,
 		);
 	}
 	const block = new Uint8Array(16);
-	const prefix = cardId.slice(0, 2);
-	const id = Uint8Array.fromHex(cardId.slice(2));
-	block.set(Uint8Array.from(prefix.split('').map(c => c.charCodeAt(0))), 0);
-	block.set(id, 2);
+	const idPrefix = id.slice(0, 2);
+	const idNumber = Uint8Array.fromHex(id.slice(2));
+	block.set(Uint8Array.from(idPrefix.split("").map((c) => c.charCodeAt(0))), 0);
+	block.set(idNumber, 2);
 	const checksum = block.reduce((acc, byte) => acc ^ byte, 0);
 	block[15] = checksum;
 	return block;
